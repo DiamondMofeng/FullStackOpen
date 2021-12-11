aa34e53e2abbcaa92a6d8ab7cd51dc02e86583f8
Mofeng <363034937@qq.com>
2021-12-11 16:37:26 +0800

    2.11

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 0 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
   ])
 
+  // { name: 'Arto Hellas', number: '040-123456', id: 0 },
+  // { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
+  // { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
+  // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
 
-
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
   const [filter, setFilter] = useState('')
 
   return (
