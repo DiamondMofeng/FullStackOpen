2a94061032ea44c566eb0efc491fdde2f897881a
Mofeng <363034937@qq.com>
2021-12-18 13:50:09 +0800

    4.2 done

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,34 +1,14 @@
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 // const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const PORT = config.PORT || 3003
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
